add cnn model file and correct error in IOU calculation

deep_qlean.py
@@ -20,89 +20,32 @@ from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2
 from keras.optimizers import SGD , Adam
 
 from object_localization_env import nature_object_env, neuron_object_env
+import deepmodel
 import ipdb
+from keras.layers import Input
+from keras.callbacks  import CSVLogger, ModelCheckpoint, TensorBoard, EarlyStopping
+from keras.models import Model
+from keras import backend as K
+from keras.optimizers import RMSprop, SGD,Adam
+import matplotlib.pyplot as plt
+from keras import metrics
+from keras import losses
 
 CONFIG = 'nothreshold'
 ACTIONS = 9 # number of valid actions
 GAMMA = 0.99 # decay rate of past observations
-OBSERVATION = 3200. # timesteps to observe before training
+OBSERVATION = 3000. # timesteps to observe before training
 EXPLORE = 3000000. # frames over which to anneal epsilon
 FINAL_EPSILON = 0.0001 # final value of epsilon
-INITIAL_EPSILON = 0.1 # starting value of epsilon
+INITIAL_EPSILON = 0.3 # starting value of epsilon
 REPLAY_MEMORY = 50000 # number of previous transitions to remember
 BATCH = 32 # size of minibatch
 FRAME_PER_ACTION = 1
-LEARNING_RATE = 1e-4
+LEARNING_RATE = 1e-5
 
 img_rows , img_cols = 80, 80
 #Convert image into Black and white
 img_channels = 1
-def vgg_model():
-    vgg16_model = Sequential()
-    vgg16_model.add(ZeroPadding2D((1, 1), input_shape=(img_rows,img_cols,img_channels)))
-    vgg16_model.add(Convolution2D(64, 3, 3, activation='relu'))
-    vgg16_model.add(ZeroPadding2D((1, 1)))
-    vgg16_model.add(Convolution2D(64, 3, 3, activation='relu'))
-    vgg16_model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    vgg16_model.add(ZeroPadding2D((1, 1)))
-    vgg16_model.add(Convolution2D(128, 3, 3, activation='relu'))
-    vgg16_model.add(ZeroPadding2D((1, 1)))
-    vgg16_model.add(Convolution2D(128, 3, 3, activation='relu'))
-    vgg16_model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    vgg16_model.add(ZeroPadding2D((1, 1)))
-    vgg16_model.add(Convolution2D(256, 3, 3, activation='relu'))
-    vgg16_model.add(ZeroPadding2D((1, 1)))
-    vgg16_model.add(Convolution2D(256, 3, 3, activation='relu'))
-    vgg16_model.add(ZeroPadding2D((1, 1)))
-    vgg16_model.add(Convolution2D(256, 3, 3, activation='relu'))
-    vgg16_model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    vgg16_model.add(ZeroPadding2D((1, 1)))
-    vgg16_model.add(Convolution2D(512, 3, 3, activation='relu'))
-    vgg16_model.add(ZeroPadding2D((1, 1)))
-    vgg16_model.add(Convolution2D(512, 3, 3, activation='relu'))
-    vgg16_model.add(ZeroPadding2D((1, 1)))
-    vgg16_model.add(Convolution2D(512, 3, 3, activation='relu'))
-    # vgg16_model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    # vgg16_model.add(ZeroPadding2D((1, 1)))
-    # vgg16_model.add(Convolution2D(512, 3, 3, activation='relu'))
-    # vgg16_model.add(ZeroPadding2D((1, 1)))
-    # vgg16_model.add(Convolution2D(512, 3, 3, activation='relu'))
-    # vgg16_model.add(ZeroPadding2D((1, 1)))
-    # vgg16_model.add(Convolution2D(512, 3, 3, activation='relu'))
-    # vgg16_model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    vgg16_model.add(Flatten())
-    vgg16_model.add(Dense(4096, activation='relu'))
-    vgg16_model.add(Dropout(0.5))
-    vgg16_model.add(Dense(4096, activation='relu'))
-    vgg16_model.add(Dropout(0.5))
-    vgg16_model.add(Dense(ACTIONS))
-    adam = Adam(lr=LEARNING_RATE)
-    vgg16_model.compile(loss='mse',optimizer=adam)
-    print("We finish building the model")
-    return vgg16_model
-def buildmodel():
-    print("Now we build the model")
-    model = Sequential()
-    model.add(Convolution2D(32, 8, 8, subsample=(4, 4), border_mode='same',input_shape=(img_rows,img_cols,img_channels)))  #80*80*4
-    model.add(Activation('relu'))
-    model.add(Convolution2D(64, 4, 4, subsample=(2, 2), border_mode='same'))
-    model.add(Activation('relu'))
-    model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode='same'))
-    model.add(Activation('relu'))
-    model.add(Flatten())
-    model.add(Dense(512))
-    model.add(Activation('relu'))
-    model.add(Dense(ACTIONS))
-   
-    adam = Adam(lr=LEARNING_RATE)
-    model.compile(loss='mse',optimizer=adam)
-    print("We finish building the model")
-    return model
 
 def trainNetwork(model,args):
     # open up a game state to communicate with emulator
@@ -122,12 +65,17 @@ def trainNetwork(model,args):
     x_t = skimage.transform.resize(x_t,(img_rows,img_rows))
     x_t = skimage.exposure.rescale_intensity(x_t,out_range=(0,255))
     s_t=x_t
+    action_history_st =[np.zeros([ACTIONS]) for i in range(10)]
+    action_history_st_1 =[np.zeros([ACTIONS]) for i in range(10)]
 
     # s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)
     #print (s_t.shape)
 
     #In Keras, need to reshape
     s_t = s_t.reshape(1, s_t.shape[0], s_t.shape[1], 1)  #1*80*80*1
+    s_at=np.array(action_history_st)
+    s_at=np.reshape(s_at,(1,-1))
+    S_Ts=[s_t,s_at]
 
     
 
@@ -145,6 +93,7 @@ def trainNetwork(model,args):
         model.load_weights("model.h5")
 
     t = 0
+    
     while (True):
         loss = 0
         Q_sa = 0
@@ -156,21 +105,20 @@ def trainNetwork(model,args):
             if random.random() <= epsilon:
                 print("----------Random Action----------")
                 action_index = random.randrange(ACTIONS)
-                # a_t[action_index] = 1
-                a_t= action_index
+                a_t[action_index] = 1
             else:
-                q = model.predict(s_t)       #input a stack of 4 images, get the prediction
+                # q = model.predict(s_t)       #input a stack of 4 images, get the prediction
+                q= model.predict(S_Ts)
                 max_Q = np.argmax(q)
                 action_index = max_Q
-                # a_t[max_Q] = 1
-                a_t= action_index
-
+                a_t[max_Q] = 1
+                # a_t= action_index
         #We reduced the epsilon gradually
         if epsilon > FINAL_EPSILON and t > OBSERVE:
             epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
 
         #run the selected action and observed next state and reward
-        x_t1, r_t, terminal = object_loc_env.localization_step(a_t)
+        x_t1, r_t, terminal = object_loc_env.localization_step(action_index)
 
         # x_t1 = skimage.color.rgb2gray(x_t1_colored)
         x_t1 = skimage.transform.resize(x_t1,(80,80))
@@ -178,10 +126,18 @@ def trainNetwork(model,args):
 
         x_t1 = x_t1.reshape(1, x_t1.shape[0], x_t1.shape[1], 1) #1x80x80x1
         s_t1=x_t1
+        
         # s_t1 = np.append(x_t1, s_t[:, :, :, :3], axis=3)
 
         # store the transition in D
-        D.append((s_t, action_index, r_t, s_t1, terminal))
+        s_at1=np.array(action_history_st_1)
+        s_at1=np.reshape(s_at1,(1,-1))
+        S_Ts1=[s_t1,s_at1]
+        D.append((S_Ts, action_index, r_t, S_Ts1,terminal))
+        # D.append((s_t, action_index, r_t, s_t1, terminal))
+        action_history_st.pop(0)
+        action_history_st.append(a_t)
+
         if len(D) > REPLAY_MEMORY:
             D.popleft()
 
@@ -190,12 +146,11 @@ def trainNetwork(model,args):
             object_loc_env.show_step()
             #sample a minibatch to train on
             minibatch = random.sample(D, BATCH)
-
-
-
-            inputs = np.zeros((BATCH, s_t.shape[1], s_t.shape[2], 1))   #32, 80, 80, 1
-            print (inputs.shape)
-            targets = np.zeros((inputs.shape[0], ACTIONS))                         #32, 2
+            # ipdb.set_trace()
+            inputs = [np.zeros((BATCH, s_t.shape[1], s_t.shape[2], 1)),\
+                        np.zeros((BATCH, s_at1.shape[1]))]   #32, 80, 80, 1
+            # print (inputs.shape)
+            targets = np.zeros((inputs[0].shape[0], ACTIONS))                         #32, 9
 
             #Now we do the experience replay
             for i in range(0, len(minibatch)):
@@ -206,9 +161,10 @@ def trainNetwork(model,args):
                 terminal = minibatch[i][4]
                 # if terminated, only equals reward
 
-                inputs[i:i + 1] = state_t    #I saved down s_t
+                inputs[0][i:i + 1] = state_t[0]    #I saved down s_t
+                inputs[1][i:i + 1] = state_t[1]
 
-                targets[i] = model.predict(state_t)  # Hitting each buttom probability
+                targets[i] = model.predict(state_t)   # Hitting each buttom probability
                 Q_sa = model.predict(state_t1)
 
                 if terminal:
@@ -220,6 +176,7 @@ def trainNetwork(model,args):
             loss += model.train_on_batch(inputs, targets)
 
         s_t = s_t1
+        S_Ts = S_Ts1
         t = t + 1
 
         # save progress every 10000 iterations
@@ -247,8 +204,17 @@ def trainNetwork(model,args):
 
 def start(args):
     # model = buildmodel()
-    model =vgg_model()
+    # model =vgg_model()
+    input_shape=(img_rows,img_cols,img_channels)
+    action_shape =(9*10,)
+    output_shape =(ACTIONS,)
+    ips,out =deepmodel.vgg_model(input_shape, action_shape,output_shape)
+    model=Model(ips,out)
+    adam = Adam(lr=LEARNING_RATE)
+    model.compile(loss='mse',optimizer=adam)
+    model.summary()
     trainNetwork(model,args)
+
 
 def main():
     parser = argparse.ArgumentParser(description='Description of your program')

object_localization_env.py
@@ -7,20 +7,26 @@ from skimage.measure import label
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from skimage.color.colorlabel import label2rgb
+def check_cornerInbox(source_box,target_box):
+	boxB= source_box
+	boxA=target_box
+	corners_of_boxB=[(boxB[0],boxB[1]),(boxB[0],boxB[3]),(boxB[2],boxB[1]),(boxB[2],boxB[3])]
+	is_intersect =False
+	for corner in corners_of_boxB:
+		x=corner[0]
+		y=corner[1]
+		if x >= boxA[0] and x<=boxA[2] and y>=boxA[1] and y<=boxA[3]:
+			is_intersect = True
+			break
+	return is_intersect
+
+
 def bb_intersection_over_union(boxA, boxB):
  		# This function code is modified based on code from http://www.pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/
 		# determine the (x, y)-coordinates of the intersection rectangle
 		# ipdb.set_trace()
 
-		corners_of_boxB=[(boxB[0],boxB[1]),(boxB[0],boxB[3]),(boxB[2],boxB[1]),(boxB[2],boxB[3])]
-		is_intersect =False
-		for corner in corners_of_boxB:
-			x=corner[0]
-			y=corner[1]
-			if x >= boxA[0] and x<=boxA[2] and y>=boxA[1] and y<=boxA[3]:
-				is_intersect = True
-				break
-		if not is_intersect:
+		if not (check_cornerInbox(boxA,boxB) or check_cornerInbox(boxB,boxA)):
 			return 0
 		xA = max(boxA[0], boxB[0])
 		yA = max(boxA[1], boxB[1])
@@ -39,7 +45,8 @@ def bb_intersection_over_union(boxA, boxB):
 		# compute the intersection over union by taking the intersection
 		# area and dividing it by the sum of prediction + ground-truth
 		# areas - the interesection area
-		iou = interArea / float(boxAArea + boxBArea - interArea)
+		# iou = interArea / float(boxAArea + boxBArea - interArea)
+		iou = interArea / float(boxAArea + boxBArea )
 	 
 		# return the intersection over union value
 		return iou
@@ -115,11 +122,11 @@ def relabel_disconnected_seglabel(seg_label):
 class object_localization_env(object):
  	def __init__(self):
  		self.data_obj =None
- 		self.init_box_x_range = [120,170]
- 		self.init_box_y_range = [120,170]
+ 		self.init_box_x_range = [45,85]
+ 		self.init_box_y_range = [45,85]
  		self.cur_bbox =None
 		self.objective_bbox =None
-		self.action_alpha=0.05
+		self.action_alpha=0.15
 		self.previous_IOU = None
 		self.tal =  0.6
 		self.eta =  3.0 
@@ -154,37 +161,34 @@ class object_localization_env(object):
 		# plt.show()
 		plt.pause(0.00001)
  	def localization_step(self,action):
+ 		self.step_counts+=1
 
  		if action != 8:
  			# ipdb.set_trace()
  			self.update_current_bbox_with_action(action)
+ 			# iou_with_objective =bb_intersection_over_union(bbox_to_4_scaler_list(self.cur_bbox),bbox_to_4_scaler_list(self.objective_bbox))
  			# ipdb.set_trace()
- 			reward = self.get_reward()
+ 			reward,iou_with_objective = self.get_reward()
  		else:
  			# for trigger action
- 			reward = self.eta \
- 			if bb_intersection_over_union(bbox_to_4_scaler_list(self.cur_bbox),bbox_to_4_scaler_list(self.objective_bbox)) > self.tal else -1*self.eta
- 		bbox_warp_image =self.get_cur_bbox_warp_image().astype(int)
- 		if action==8:
+ 			iou_with_objective =bb_intersection_over_union(bbox_to_4_scaler_list(self.cur_bbox),bbox_to_4_scaler_list(self.objective_bbox))
+ 			reward = self.eta if  iou_with_objective > self.tal else -1*self.eta
+ 
+ 		if action==8 or iou_with_objective ==0 or self.step_counts >60:
  			terminal = True
  			self.init_starting_box()
  		else:
  			terminal = False
- 		self.step_counts+=1
- 		if self.step_counts >20:
- 			self.init_starting_box()
- 			terminal = True
- 		# self.show_step()
+ 		if iou_with_objective ==0:
+ 			print('terminated loc as IOU ==0')
+ 		bbox_warp_image =self.get_cur_bbox_warp_image()#.astype(int)
  		return bbox_warp_image,reward, terminal
- 		# warp_image(image, bbox):
- 	# def get_cur_bbox_warp_image():
- 	# 	pass
-
+ 
  	def get_reward(self):
  		cur_iou = bb_intersection_over_union(bbox_to_4_scaler_list(self.objective_bbox),bbox_to_4_scaler_list(self.cur_bbox))
  		reward = 1 if (cur_iou -self.previous_IOU)>0 else -1
  		self.previous_IOU =cur_iou
- 		return reward
+ 		return reward,cur_iou
 
  	def update_current_bbox_with_action(self,action):
  		alpha_w = int(round(self.action_alpha *(self.cur_bbox[1][0]-self.cur_bbox[0][0])))
@@ -281,6 +285,7 @@ class neuron_object_env(object_localization_env):
 	def read_one_image(self):
 		slice_idx =50
 		image,seg_label,all_bbox  =	self.data_obj.get_image_with_boundingBox(slice_idx)
+		# ipdb.set_trace()
 		self.all_bbox	=	all_bbox
 		self.seg_label  =   seg_label
 		self.x_size 	=	image.shape[0]
@@ -293,6 +298,15 @@ class neuron_object_env(object_localization_env):
 		y_top 			=	random.randint(0,self.y_size-self.init_box_y_range[1])
 		y_bottom 		= 	y_top+random.randint(self.init_box_y_range[0], self.init_box_y_range[1])
 		self.cur_bbox 	=	[[x_left,y_top],[x_right,y_bottom]]
+	def find_center_label(self,bbox):
+		cent_x= int(round(bbox[0] + (bbox[2]-bbox[0])/2.0))
+		cent_y= int(round(bbox[1] + (bbox[3]-bbox[1])/2.0))
+		lb=self.seg_label[cent_x,cent_y]
+		while lb==0:
+			n_x= cent_x+random.randrange(-15,15)
+			n_y= cent_y+random.randrange(-15,15)
+			lb=self.seg_label[n_x,n_y]
+		return lb
 
 	def init_starting_box(self):
 		
@@ -302,13 +316,16 @@ class neuron_object_env(object_localization_env):
 		# ipdb.set_trace()
 		all_box_list = self.all_bbox.values()
 		all_box_keys = self.all_bbox.keys()
-		while True:
-			self.get_random_start_bbox()
-			idx=index_of_largest_IOU(self.cur_bbox,all_box_list)
-			if idx >-1:
-				break
-		self.objective_bbox =all_box_list[idx]
-		largest_lb=all_box_keys[idx]
+		self.get_random_start_bbox()
+		lb=self.find_center_label(bbox_to_4_scaler_list(self.cur_bbox))
+		self.objective_bbox =self.all_bbox[lb]
+		# while True:
+		# 	self.get_random_start_bbox()
+		# 	idx=index_of_largest_IOU(self.cur_bbox,all_box_list)
+		# 	if idx >-1:
+		# 		break
+		# self.objective_bbox =all_box_list[idx]
+		# largest_lb=all_box_keys[idx]
 		# ipdb.set_trace()
 		self.previous_IOU   = bb_intersection_over_union(bbox_to_4_scaler_list(self.cur_bbox),
 														bbox_to_4_scaler_list(self.objective_bbox))
@@ -326,7 +343,8 @@ class RL_data(object):
 class RL_neuron_data(RL_data):
 	def __init__(self):
 		super(RL_neuron_data,self).__init__()
-		self.data_file='./data/snemi3d_train_full_stacks_added_segLabel_v1.h5'
+		self.data_file='./data/snemi3d_train_full_stacks_added_segLabel_v1_scaled.h5'
+
 		h5f=h5py.File(self.data_file,'r')
 		self.images=h5f['data']
 		self.seg_labels=h5f['seg_label']
@@ -415,12 +433,14 @@ def test_env():
 		# 
 		warp_x=np.transpose(warp_x)
 		image =np.transpose(image)
+		# ipdb.set_trace()
 		ax[0].clear()
-		ax[0].imshow(image,cmap='gray')
+		ax[0].imshow(label_rgb_im,cmap='gray')
+		# ax[0].imshow(image,cmap='gray')
 		# ipdb.set_trace()
 		ax[1].clear()
 		ax[1].imshow(warp_x,cmap='gray')
-		# ax.imshow(label_rgb_im,cmap='gray')
+		
 		# ax=plt.imshow(image,cmap='gray')
 		ppxy,w,h=conver_bbox_to_xy_width_height(bbox)
 		ppxy_o,w_o,h_o=conver_bbox_to_xy_width_height(env.objective_bbox)